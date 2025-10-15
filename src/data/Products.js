e4539d590e2fdf8b621b898dbe58b91e2947a365
Nihiliister <bencasti9031@gmail.com>
2025-10-15 17:11:41 -0300

    Update Products.js

--- src/data/Products.js
+++ src/data/Products.js
@@ -3,14 +3,15 @@ const products = [
     id: 1,
     name: 'Proyecto "PENNE"',
     description: 'Este proyecto fue uno de los primeros que hize mientras estudiaba analista programador',
-    image: 'src/assets/proyectos/ProyectoExpress.webp'
+    image: 'proyectos/ProyectoExpress.webp'
   },
   {
     id: 2,
     name: 'Proyecto NoLimits',
     description: 'Estuve involucrado en el front end de este proyecto y hicimos la pagina web de esta',
-    image: 'src/assets/proyectos/ProyectoNoLimits.webp'
+    image: 'proyectos/ProyectoNoLimits.webp'
   }
 ];
+
 
 export default products;
